8ad48e3f1854af94f24d08e33b55843c95035008
Vlada <jagarioendl@gmail.com>
2020-06-28 17:59:53 +0300

    first commit

---
+++ src/main/java/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    public  void startTreatment(Patient patient) {
+        System.out.println("Получить историю болезней пациента");
+        System.out.println("Сделать записи по приему пациента");
+        System.out.println("Получить результаты анализов");
+        System.out.println("Провести осмотр");
+        System.out.println("Назначить лечение");
+        System.out.println("Отвечать на вопросы");
+        System.out.println("Назначить новую встречу");
+    }
+}
